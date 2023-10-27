4f7d6c090d45cbefbab334685ccd56574e6fe42e
Nathan Byrd <techy1@gmail.com>
2023-10-27 21:19:52 +0000

    Added tests and initial files

---
+++ __tests__/index.test.js
@@ -0,0 +1,4 @@
+test('bbsDoorRunner should be an object', () => {
+    const bbsDoorRunner = require('../index');
+    expect(typeof bbsDoorRunner).toBe('object');
+});
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import required modules
+const fs = require('fs');
+const path = require('path');
+
+// Define module
+const bbsDoorRunner = {
+  // Define properties and methods here
+};
+
+// Export module
+module.exports = bbsDoorRunner;
